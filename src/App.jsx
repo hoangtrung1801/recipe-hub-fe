2e8f44f8bad329b6f21fa4cbce917f4f9fe75d7c
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-09-21 16:02:52 +0700

    feat: 🎸 init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-lg text-green-600">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
