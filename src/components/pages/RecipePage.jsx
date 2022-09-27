8a459f09a3444a9873a84a399b25d0737a880c87
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-09-27 14:46:32 +0700

    feat: 🎸 finish sketching recipe page

--- src/components/pages/RecipePage.jsx
+++ src/components/pages/RecipePage.jsx
@@ -1,3 +1,5 @@
+import { GitFork, Star } from "phosphor-react";
+import Button from "../buttons/Button";
 import Counter from "../Counter";
 
 const ingredients = [
@@ -8,21 +10,21 @@ const ingredients = [
         // detail: 'sliced'
     },
     {
-        name: "Mushrooms",
-        amount: 200,
+        name: "Spinach",
+        amount: 60,
         unit: "g",
         // detail: 'sliced'
     },
     {
-        name: "Mushrooms",
-        amount: 200,
-        unit: "g",
+        name: "Garlic cloves",
+        amount: 2,
+        unit: "",
         // detail: 'sliced'
     },
     {
-        name: "Mushrooms",
-        amount: 200,
-        unit: "g",
+        name: "Olive oil",
+        amount: 1,
+        unit: "tbsp",
         // detail: 'sliced'
     },
 ];
@@ -59,6 +61,38 @@ const nutrions = [
         name: "Sugar",
         amount: 6.6,
         unit: "g",
+    },
+];
+
+const instructions = [
+    {
+        id: 1,
+        instruction:
+            "Heat a frying pan on high heat, once hot add the olive oil along with mushroom and garlic – cook for ~4 mins",
+    },
+    {
+        id: 2,
+        instruction: "Add the spinach to the frying pan and cook until wilted",
+    },
+    {
+        id: 3,
+        instruction:
+            " Add the walnuts, spinach, basil, garlic, nutritional yeast, lemon juice, salt, pepper and olive oil to a pestle and mortar – mash up the ingredients until it forms a smooth paste",
+    },
+    {
+        id: 4,
+        instruction:
+            "Transfer the mushroom mixture to a bowl, spread pesto on one side of the bread and place in the frying pan, pesto side up – toast for ~3 mins",
+    },
+    {
+        id: 5,
+        instruction:
+            "Take the bread out of the frying pan, place the mushroom mixture on each slice of bread and layer sliced tomato on top",
+    },
+    {
+        id: 6,
+        instruction:
+            "Place back into the frying pan on the un-toasted side and toast for ~3 mins – remove and cut in half",
     },
 ];
 
@@ -158,28 +192,94 @@ const RecipeDetail = () => {
                     Cook <span className="font-medium">25m</span>
                 </p>
             </div>
+            <div className="border-y border-dark-0">
+                <div className="flex space-x-4 py-6">
+                    <Button variant="light" className="space-x-2">
+                        <Star className="text-lg" />
+                        <span>Star</span>
+                    </Button>
+                    <Button variant="light" className="space-x-2">
+                        <GitFork className="text-lg" />
+                        <span>Fork</span>
+                    </Button>
+                </div>
+            </div>
             <div>
                 <p>Nutrion per serving</p>
-                <div className="mt-4 grid grid-cols-6 gap-x-2">
+                <div className="mt-4 flex flex-wrap gap-2">
                     {nutrions.map((nutrion) => (
                         <div
                             key={nutrion.name}
-                            className="flex flex-col items-center rounded-full bg-primary-300 px-2 pt-2 pb-5"
+                            className="flex flex-col items-center rounded-full bg-primary-300 px-2 pt-2 pb-6 text-dark-1"
                         >
-                            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary-100">
+                            <div className="flex h-[4.5rem] w-[4.5rem] items-center justify-center rounded-full bg-primary-100">
                                 <span>
                                     {nutrion.amount}
                                     {nutrion.unit}
                                 </span>
                             </div>
-                            <div className="mt-2 text-sm">
+                            <div className="mt-4 text-sm">
                                 <span>{nutrion.name}</span>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
-            <div></div>
+            <div>
+                <h3 className="text-3xl font-bold">Instructions</h3>
+                <div className="mt-8 space-y-6">
+                    {instructions.map((instruction) => (
+                        <div key={instructions.id} className="flex space-x-6">
+                            <div className="">
+                                <div className="flex h-12 w-12 items-center justify-center rounded-full bg-primary-300">
+                                    <span className="text-xl font-medium">
+                                        {instruction.id}
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="text-lg">
+                                <p>{instruction.instruction}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <Button className="w-full py-5 text-2xl font-medium">
+                    Start cooking
+                </Button>
+            </div>
+            <div>
+                <hr />
+            </div>
+            <div>
+                <div className="flex space-x-6">
+                    <div className="basis-24">
+                        <div className="aspect-square overflow-hidden rounded-full">
+                            <img
+                                src="https://randompicturegenerator.com/img/people-generator/gab5efb99b1f5b1ac18f8f302c793aaa28f5e881eb21d8ae4f7f64f55d3d800a5c77426e67b2e74c5e8d7c1787c13369f_640.jpg"
+                                alt="avatar"
+                                className="h-full w-full object-cover"
+                            />
+                        </div>
+                    </div>
+                    <div className="mt-2 flex-1">
+                        <p className="font-semibold">hoangtrung1801</p>
+                        <p className="mt-2">
+                            If you enjoyed this recipe share it with your
+                            friends and family who might also enjoy it
+                        </p>
+                        {/* <div className="mt-4 flex space-x-3">
+                            <button className="circle-outline text-xl">
+                                <Star />
+                            </button>
+                            <button className="circle-outline text-xl">
+                                <GitFork />
+                            </button>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
