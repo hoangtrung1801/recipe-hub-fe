05efb55fe6c3d266a8a299b45c9134e78305185c
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-10-04 21:02:08 +0700

    fix card component recipes page

--- src/components/pages/RecipesPage.jsx
+++ src/components/pages/RecipesPage.jsx
@@ -60,8 +60,10 @@ const RecipesPage = () => {
                             </h1>
                         </div>
                         <div className=" gridList rn:w-[98%] mx-auto grid w-[98%] grid-cols-2 gap-4 md:w-[95%] md:grid-cols-3 md:gap-6 lg:w-[90%] lg:grid-cols-4 lg:gap-10  ">
-                            <div className=" article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrapper  h-[240px] bg-[url(https://images.prismic.io/stryve/338b523d-73cc-439a-acd0-d02eaff6ba7e_nutty-apple-date-porridge.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/338b523d-73cc-439a-acd0-d02eaff6ba7e_nutty-apple-date-porridge.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat md:h-[270px] lg:h-[360px]"></div>
+                            <div className=" article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5  lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrapper h-[240px] bg-[url(https://images.prismic.io/stryve/338b523d-73cc-439a-acd0-d02eaff6ba7e_nutty-apple-date-porridge.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/338b523d-73cc-439a-acd0-d02eaff6ba7e_nutty-apple-date-porridge.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
 
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl ">
                                     <UnderlineLink>
@@ -90,8 +92,10 @@ const RecipesPage = () => {
                                 </div>
                             </div>
 
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrapper  h-[240px] overflow-hidden bg-[url(https://images.prismic.io/stryve/2be94b3a-3084-4aa0-817d-7af87596bd74_pulled-jackfruit-loaded-fries.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/2be94b3a-3084-4aa0-817d-7af87596bd74_pulled-jackfruit-loaded-fries.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrapper h-[240px] overflow-hidden bg-[url(https://images.prismic.io/stryve/2be94b3a-3084-4aa0-817d-7af87596bd74_pulled-jackfruit-loaded-fries.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/2be94b3a-3084-4aa0-817d-7af87596bd74_pulled-jackfruit-loaded-fries.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -117,8 +121,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrapper  h-[240px] overflow-hidden bg-[url(https://images.prismic.io/stryve/3d40b6ec-7d41-4d48-bd3a-5bf78ca5b303_tomato-pesto-pasta.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/3d40b6ec-7d41-4d48-bd3a-5bf78ca5b303_tomato-pesto-pasta.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5  lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrapper h-[240px] overflow-hidden bg-[url(https://images.prismic.io/stryve/3d40b6ec-7d41-4d48-bd3a-5bf78ca5b303_tomato-pesto-pasta.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/3d40b6ec-7d41-4d48-bd3a-5bf78ca5b303_tomato-pesto-pasta.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -144,8 +150,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/fad9e994-32e9-4511-b620-5b3a6287009f_mushroom-spinach-pesto-toasted-sandwich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/fad9e994-32e9-4511-b620-5b3a6287009f_mushroom-spinach-pesto-toasted-sandwich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/fad9e994-32e9-4511-b620-5b3a6287009f_mushroom-spinach-pesto-toasted-sandwich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/fad9e994-32e9-4511-b620-5b3a6287009f_mushroom-spinach-pesto-toasted-sandwich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -171,8 +179,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/7ac06668-07b3-4dbb-ac42-850aa27a7ec3_mushroom-stroganoff.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/7ac06668-07b3-4dbb-ac42-850aa27a7ec3_mushroom-stroganoff.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/7ac06668-07b3-4dbb-ac42-850aa27a7ec3_mushroom-stroganoff.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/7ac06668-07b3-4dbb-ac42-850aa27a7ec3_mushroom-stroganoff.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -198,8 +208,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/6888281e-7d9d-4f83-abcb-6c1ac5eaf88c_chickpea-mayo-sandwhich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/6888281e-7d9d-4f83-abcb-6c1ac5eaf88c_chickpea-mayo-sandwhich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/6888281e-7d9d-4f83-abcb-6c1ac5eaf88c_chickpea-mayo-sandwhich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/6888281e-7d9d-4f83-abcb-6c1ac5eaf88c_chickpea-mayo-sandwhich.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -225,8 +237,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/079be1b2-fb5d-4e64-9a98-5f5f231e8c63_mushroom-kale-macaroni.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/079be1b2-fb5d-4e64-9a98-5f5f231e8c63_mushroom-kale-macaroni.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/079be1b2-fb5d-4e64-9a98-5f5f231e8c63_mushroom-kale-macaroni.png?auto=compress,format&fm=webp&lossless=false&q=75&w=375%20375w,%20https://images.prismic.io/stryve/079be1b2-fb5d-4e64-9a98-5f5f231e8c63_mushroom-kale-macaroni.png?auto=compress,format&fm=webp&lossless=false&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
@@ -252,8 +266,10 @@ const RecipesPage = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 duration-500 hover:scale-105 lg:mb-14">
-                                <div className="imgWrappe roverflow-hidden h-[240px] bg-[url(https://images.prismic.io/stryve/93d08668-16df-4395-807f-923c43a3c220_griddled-courgette-asparagus-pasta.png?auto=compress,format&q=75&w=375%20375w,%20https://images.prismic.io/stryve/93d08668-16df-4395-807f-923c43a3c220_griddled-courgette-asparagus-pasta.png?auto=compress,format&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-500 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                            <div className="article relative  flex min-w-full cursor-pointer flex-col gap-8 overflow-hidden  pb-5 lg:mb-14">
+                                <div className="overflow-hidden">
+                                    <div className="imgWrapper h-[240px] bg-[url(https://images.prismic.io/stryve/93d08668-16df-4395-807f-923c43a3c220_griddled-courgette-asparagus-pasta.png?auto=compress,format&q=75&w=375%20375w,%20https://images.prismic.io/stryve/93d08668-16df-4395-807f-923c43a3c220_griddled-courgette-asparagus-pasta.png?auto=compress,format&q=75&w=450%20450w)] bg-cover bg-center bg-no-repeat duration-300 duration-500 hover:scale-105 hover:scale-105 md:h-[270px] lg:h-[360px]"></div>
+                                </div>
                                 <div className="textWrapper  text-left text-xl font-medium lg:text-3xl">
                                     <UnderlineLink>
                                         {" "}
