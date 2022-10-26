890330d3459b7c10aaee3c57728382144e592d80
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-10-27 06:56:37 +0700

    fix follow instruction 2

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -13,7 +13,7 @@ const Input = (props) => {
                 name="text"
                 type="text"
                 autoComplete="email"
-                className="relative block w-full   appearance-none rounded-none  border border-gray-300 bg-primary-100 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-dark-0 focus:outline-none focus:ring-dark-0 sm:text-sm"
+                className="relative block w-full appearance-none rounded-none  border border-gray-300 bg-primary-100 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-dark-0 focus:outline-none focus:ring-dark-0 sm:text-sm"
                 placeholder={placeHolder}
                 value={state}
                 onChange={(e) => setState(e.target.value)}
