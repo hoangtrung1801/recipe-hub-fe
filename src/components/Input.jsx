8bbb2c03b0effad254678cf61193ef53a2c09e0c
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-11-15 20:22:52 +0700

    resolve conflicts

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef(({ label, ...props }, ref) => {
                 <label>{label}</label>
                 <input
                     ref={ref && ref}
-                    className="relative  block w-full appearance-none rounded-none  border border-gray-300 bg-primary-100 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-dark-0 focus:outline-none focus:ring-dark-0 sm:text-sm"
+                    className="relative block w-full appearance-none rounded-none  border border-gray-300 bg-primary-100 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-dark-0 focus:outline-none focus:ring-dark-0 sm:text-sm"
                     {...props}
                 />
             </div>
